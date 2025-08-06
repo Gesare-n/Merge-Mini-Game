25a4c78bbdd158d4540928155e1d825a6dd601b2
Moh. Yosry Zorainy <mohamedu3@gmail.com>
2025-08-06 11:46:30 +0300

    First APK - Build v1.0.1
    - Successfully build the first APK of the game version 1.0.1.

--- .utmp/RelWithDebInfo/6qv4m135/armeabi-v7a/cmake_install.cmake
+++ .utmp/RelWithDebInfo/6qv4m135/armeabi-v7a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/digit/OneDrive/Jobs/GameUpElite/7.Merge Game Project/Merge-Mini-Game/Library/Bee/Android/Prj/Mono2x/Gradle/unityLibrary/src/main/cpp
+# Install script for directory: C:/Users/digit/OneDrive/Jobs/GameUpElite/7.Merge Game Project/Merge-Mini-Game/Library/Bee/Android/Prj/IL2CPP/Gradle/unityLibrary/src/main/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
