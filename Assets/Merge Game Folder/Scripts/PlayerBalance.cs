8be4ae6700f8caafac5dc1d29228f4db83d6ad2c
Moh. Yosry Zorainy <mohamedu3@gmail.com>
2025-08-20 15:44:49 +0300

    Currency is Added and Tracked
    - Gems (currency) introduced to the game. They are tracked and all operations (functions) related to it are in PlayerBalance.cs.
    - Added some on screen Cheats (buttons) to increase or decrease the balance by 50 or reset it.
    - Everything is saved and loaded. So, the value is preserved between playing sessions.
    - All these are on the GitHub branch "monetization_yosry" created from "merge-visuals"

--- Assets/Merge Game Folder/Scripts/PlayerBalance.cs
+++ Assets/Merge Game Folder/Scripts/PlayerBalance.cs
@@ -1,16 +1,80 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerBalance : MonoBehaviour
 {
+    public static PlayerBalance instance;
+
+    //public UserInterface UI;
+
+    [SerializeField] private int currentBalance = 0;
+
+    private string gems = "g_01";           // Obfuscated key name for gems
+    public TextMeshProUGUI balanceText;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentBalance = LoadBalance();
+        balanceText.text = currentBalance.ToString();
+        //UI = GameObject.FindGameObjectWithTag("UI").GetComponent<UserInterface>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void AddToBalance (int amount)
+    {
+        currentBalance += amount;
+        SaveBalance();
+        balanceText.text = currentBalance.ToString();
+    }
+
+    public void RemoveFromBalance (int amount)
+    {
+        currentBalance -= amount;
+        SaveBalance();
+        balanceText.text = currentBalance.ToString();
+    }
+
+    public void ResetBalance()
+    {
+        currentBalance = 0;
+        SaveBalance ();
+        balanceText.text = currentBalance.ToString();
+    }
+
+    void SaveBalance()
+    {
+        PlayerPrefs.SetInt (gems, currentBalance);
+    }
+
+    int LoadBalance()
+    {
+        int balance = PlayerPrefs.GetInt(gems);
+        return (balance);
+    }
+
+    public void IncreaseBalanceCheat()
+    {
+        AddToBalance(50);
+    }
+
+    public void DecreaseBalanceCheat()
+    {
+        RemoveFromBalance(50);
     }
 }
